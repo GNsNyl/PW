129f59c83e5d02f0d1e086aaaa296ff74f5cb204
lyunsong <110934662+GNsNyl@users.noreply.github.com>
2023-03-31 13:03:57 -0400

    new page

---
+++ js/wordcloud.js
@@ -0,0 +1,355 @@
+
+
+const myWords=[
+    {
+        "word": "SMART",
+        'tcolor':'#000000',
+        'rot': '1',
+        'x':0.92,
+        'y':0.1,
+        'fill':'#ffffff',
+        'border':0,
+        'borderColor':'#00ffd0',
+        "font": "Libre_Barcode_39",
+        "size": "200",
+        "url":'dist/info.html'
+
+    },
+    {
+        "word": "DATA",
+        'tcolor':'#00ffd0',
+        'rot': '1',
+        'x':0.2,
+        'y':0.16,
+        'fill':'#ffffff',
+        'border':3,
+        'borderColor':'#000000',
+        "font": "Press_Start_2P",
+        "size": "89",
+        "link":'<p>I do data analysis using Python and arcGIS<br>I do data viz using Javascript Python and arcGIS<br>click to see</p>',
+        "url":'dist/digital.html'
+    },
+    {
+        "word": "YL-bio",
+        'tcolor':'#000000',
+        'rot': '3',
+        'x':0.65,
+        'y':0.9,
+        'fill':'#ffffff',
+        'border':'solid',
+        'borderColor':'#00ffd0',
+        "font": "Geostar_Fill",
+        "size": "10",
+        "link":"<p>点开链接查看 Click to see my bio</p>",
+        "url":'dist/info.html'
+    },
+    {
+        "word": "MAPPING",
+        'tcolor':'#000000',
+        'rot': '0',
+        'x':0.01,
+        'y':0.23,
+        'fill':'#ffffff',
+        'border':2,
+        'borderColor':'#00ffd0',
+        "font": "Press_Start_2P",
+        "size": "30",
+        "link":'<p>I do mapping and data viz</p>',
+        "url":'dist/digital.html'
+
+    },
+    {
+        "word": "urBAN",
+        'tcolor':'#000000',
+        'rot': '0',
+        'x':0.01,
+        'y':0.93,
+        'fill':'#ffffff',
+        'border':'solid',
+        'borderColor':'#00ffd0',
+        "font": "Press_Start_2P",
+        "link":'<p>我做城市设计<br>I am trained as an urban designer<br>点击查看详情 click to see</p>',
+
+        "size": "10",
+    },
+    {
+        "word": "建筑",
+        'tcolor':'#000000',
+        'rot': '0',
+        'x':0.07,
+        'y':0.93,
+        'fill':'#ffffff',
+        'border':'solid',
+        'borderColor':'#00ffd0',
+        "font": "Noto_Sans_TC_thin",
+        "size": "20",
+        "link":'<p>我学建筑<br>I am trained as an architect<br>点击查看详情 click to see</p>',
+
+        "url":'dist/arch.html'
+    },
+    {
+        "word": "CODE",
+        'tcolor':'#000000',
+        'rot': '0',
+        'x':0.11,
+        'y':0.9,
+        'fill':'#ffffff',
+        'border':1,
+        'borderColor':'#00ffd0',
+        "font": "Rubik_80s_Fade",
+        "size": "300",
+        "link":"<p>PYTHON: pandas, geopandas, networkx, matlib, scikit-learn, numpy, tensorflow, matplotlib, seaborn" +
+            "<br>VISUAL: Adobe Illustrator, Photoshop, Indesign, AfterEffects, Gephi...GIS" +
+            "<br>3D: Rhino GH, SketchUp, Autodesk CAD, ArchiCAD</p>",
+        "url":'dist/digital.html'
+
+    },
+    {
+        "word": "可视化",
+        'tcolor':'#00ffd0',
+        'rot': '0',
+        'x':0.35,
+        'y':0.78,
+        'fill':'#ffffff',
+        'border':5,
+        'borderColor':'#000000',
+        "font": "Noto_Sans_TC_black",
+        "size": "150",
+        "link":'<p>我做[互动式]数据可视化用Javascript，PYTHON和ARCGIS<br>点击查看详情</p>',
+
+        "url":'dist/digital.html'
+
+    },
+    {
+        "word": "laBOR",
+        'tcolor':'#00ffd0',
+        'rot': '0',
+        'x':0.55,
+        'y':0.58,
+        'fill':'#ffffff',
+        'border':5,
+        'borderColor':'#000000',
+        "font": "Press_Start_2P",
+        "size": "150",
+    },
+    {
+        "word": "DESIGN",
+        'tcolor':'#00ffd0',
+        'rot': '0',
+        'x':0.29,
+        'y':0.2,
+        'fill':'#ffffff',
+        'border':6,
+        'borderColor':'#000000',
+        "font": "Rampart_One",
+        "size": "150",
+        "link":'I do archietcture, urban, and web design',
+        "url":'dist/digital.html'
+
+    },
+    {
+        "word": "城市",
+        'tcolor':'#000000',
+        'rot': '1',
+        'x':0.51,
+        'y':0.1,
+        'fill':'#ffffff',
+        'border':3,
+        'borderColor':'#00ffd0',
+        "font": "ZCOOL_XiaoWei",
+        "link":"<p>我做城市设计和数据分析</p>",
+        "size": "200",
+        "url":'dist/digital.html'
+
+    },
+    {
+        "word": "数据",
+        'tcolor':'#000000',
+        'rot': '0',
+        'x':0.21,
+        'y':0.7,
+        'fill':'#ffffff',
+        'border':2,
+        'borderColor':'#00ffd0',
+        "font": "ZCOOL_XiaoWei",
+        "size": "100",
+        "link":'<p>我做数据[分析]用PYTHON和ARCGIS<br>点击查看详情</p>',
+        "url":'dist/digital.html'
+
+
+    },
+
+
+    {
+        "word": "critical theory",
+        'tcolor':'#00ffd0',
+        'rot': '0',
+        'x':0.77,
+        'y':0.9,
+        'fill':'#ffffff',
+        'border':5,
+        'borderColor':'#000000',
+        "font": "Fredericka_the_Great",
+        "size": "50",
+        "link":'<p>我也写东西 I am into critical theories on tech phil hist</p>',
+        "url":'dist/word.html'
+
+    },
+    {
+        "word": "WEB DEV",
+        'tcolor':'#00ffd0',
+        'rot': '0',
+        'x':0.7,
+        'y':0.7,
+        'fill':'#ffffff',
+        'border':3,
+        'borderColor':'#000000',
+        "font": "Monoton",
+        "size": "90",
+        "link":'<p>我写前端【微博GL, D3, P5, THREE...】 I write website<br>点击看详情 click to see</p>',
+        "url":'dist/digital.html'
+
+    },
+    {
+        "word": "DIgital ARt",
+        'tcolor':'#000000',
+        'rot': '1',
+        'x':0.01,
+        'y':0.27,
+        'fill':'#ffffff',
+        'border':3,
+        'borderColor':'#000000',
+        "font": "Hnalei",
+        "link":'<p>at the intersection of ML and art<br>click to see</p>',
+        "size": "130",
+        "url":'dist/digital.html'
+
+    },
+]
+
+// const fontL=['Hnalei','Monoton','Press_Start_2P','Fredericka_the_Great','Libre_Barcode_39','Rampart_One','Rubik_80s_Fade','Zen_Kaku_Gothic_New']
+
+// set the dimensions and margins of the graph
+var cldmargin = {top: 0, right: 0, bottom: 0, left: 0},
+    cldwidth = document.getElementById('word-cloud-container').getBoundingClientRect().width - cldmargin.left - cldmargin.right,
+    cldheight = document.getElementById('word-cloud-container').getBoundingClientRect().height - cldmargin.top - cldmargin.bottom;
+let wordCldsvg,wordsvg;
+function draw(w,h,words) {
+    var cldTooltipDiv = d3.select("#word-tool")
+        .append("div")
+        .style("opacity", 1)
+        .attr("id", "tooltip-word")
+        .attr("class", "tooltip")
+        .style("background-color", "white");
+
+    var cldmouseover= function(d) {
+        cldTooltipDiv
+            .html(
+                d.link
+            );
+
+        cldTooltipDiv
+            // .transition()
+            // .duration(50)
+            .style('left', d3.event.pageX +20+ 'px')
+            .style('top', d3.event.pageY + 'px')
+            // .style("opacity", 1)
+            .style ("display" , 'block');
+        ;
+
+        d3.select(this)
+            // .transition()
+            // .duration(150)
+            .style("stroke", function(d){
+                return d.borderColor
+            })
+
+    };
+    var cldmousemove= function(d) {
+
+        cldTooltipDiv
+            .transition()
+            .duration(50)
+            .style('left', d3.event.pageX +20+ 'px')
+            .style('top', d3.event.pageY + 'px')
+            .style ("display" , 'block')
+        ;
+            // .style('background-color', "#ffffff");
+            // .style("opacity", 1);
+
+    };
+    var cldmouseleave = function(d) {
+        cldTooltipDiv
+            .transition ()
+            .duration (150)
+            .style ("display" , 'none');
+
+        d3.select (this)
+            // .transition ()
+            // .duration (150)
+            .style("stroke", "none")
+    };
+// append the svg object to the body of the page
+    wordCldsvg = d3.select ("#word-cloud-container").append ("svg")
+        .attr ("width" , w + cldmargin.left + cldmargin.right)
+        .attr ("height" , h + cldmargin.top + cldmargin.bottom);
+
+    wordsvg=wordCldsvg.append ("g")
+        .selectAll ("text")
+        .data (words)
+        .enter ()
+        .append ("text")
+        .attr('stroke-width',function(d){
+            return d.border
+        })
+
+        .attr('class','bigtext')
+
+        .style ("font-size" , function(d) {
+            return d.size/1000*h
+        })
+
+        .style ("fill" , function(d) {
+            return d.tcolor
+        })
+        .style ("opacity" , 1)
+        .attr ("text-anchor" , "right")
+        .attr("class", "node")
+
+        // .attr ("display" , "flex")
+        .attr ("font-family" , function(d) {
+            return d.font
+        })
+        .attr ("transform" , function(d) {
+            return "translate(" + [d.x * w - 10 , d.y * h] + ")rotate(" + d.rot * 90 + ")";
+        })
+        .text (function(d) {
+            return d.word;
+        })
+        .on("click",function(d){
+            window.open(d.url, '_blank')})
+        .on("mouseover", cldmouseover)
+        .on("mousemove", cldmousemove)
+        .on("mouseleave", cldmouseleave)
+    ;
+}
+
+draw(cldwidth,cldheight,myWords)
+
+
+// Redraw based on the new size whenever the browser window is resized.
+//
+// function updateWindow(){
+//     x = document.getElementById('word-cloud-container').getBoundingClientRect().width - cldmargin.left - cldmargin.right,
+//     y = document.getElementById('word-cloud-container').getBoundingClientRect().height - cldmargin.top - cldmargin.bottom;
+//     // cldwidth = document.getElementById('word-cloud-container').getBoundingClientRect().width - cldmargin.left - cldmargin.right,
+//     // cldheight = document.getElementById('word-cloud-container').getBoundingClientRect().height - cldmargin.top - cldmargin.bottom;
+//     // draw(cldwidth,cldheight,myWords)
+//     d3.select ("#word-cloud-container").attr("width", x).attr("height", y);
+//     wordsvg.attr("width", x).attr("height", y);
+//     wordsvg.selectAll('text')
+//         .
+//
+// }
+
+// window.addEventListener('resize', updateWindow);
